e65f0cada13dc82c769fde443717ab9193ffa128
leon <leon.rambing10@gmail.com>
2023-08-15 01:02:16 +0800

    exercise.js

---
+++ exercise/exercise.js
@@ -0,0 +1,12 @@
+// Tugas kelompok bagian Penjumlahan , Leon
+
+function firstMessage(msg1, msg2) {
+  // msg = parameter
+  return msg1 + " " + msg2; // msg + Spasi + msg
+}
+
+const msg1 = "Hello"; // string word
+const msg2 = "World"; // string word
+const result = firstMessage(msg1, msg2); // msg 1 for "Hello" msg 2 for "World"
+
+console.log(result); // result = Hello World
